5ca8eb92e95dac5c171007d064cf4cb0bbd89aef
trex414 <58962227+trex414@users.noreply.github.com>
2022-04-06 15:03:24 -0400

    Update Teacher.java

--- Teacher.java
+++ Teacher.java
@@ -1,65 +1,121 @@
-// this is nowhere done and needs a lot of work. i will most likley change it tomorrow.
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.ArrayList;
 
 public class Teachers extends User {
-    private String course;
-    
-    public Teachers(String username, String password, String course) {
-        super(username, password);
-        this.course = course;
+    private ArrayList<Course> courses = new ArrayList<Course>();
+    public Teachers(String username, String password) {
+        super(username, password, true);
     }
 
-    // add quiz. needs the quiz name, needs a quiz object to get to the questions
-    // we can make this where it makes one question on a quiz and keeps getting asked or,
-    // it makes the whole quiz\
-  
-  //questions:
-  //how does course work with quizzes and questions, what do i need from the user for creating the class, removing the class, and modifying the class.
+    /** do we want to make each teacher have all their quizzes?
+     *  how do we want to get files printed for persistence? we could have a Writing function in each file
+     *  we can have each quiz print into a quizzes file, students name file, each student with their quiz, same with teachers courses
+     *  each course could write the quizzes int their course 
+     */
 
-    //be able to add quizes to a course the teach can only acces it
-    public String addQuiz(String quizName, String[] quizObject) {
-        if (!quizName.equalsIgnoreCase(course.getQuizzes().getname())) {
-            course.getQuizzes.setquizName(quizName);
-            course.getQuizzes.setQuestions(quizObject);
-            return "Quiz Added";
-        } else {
-            return "Error, Quiz already created";
-        }
+    //be able to add quizzes to a course the teach can only acces it
+    public ArrayList<Course> getCourses() {
+        return courses;
     }
-    public String modifyQuiz(String quizName, String newQuizName) {
-        if (quizName.equalsIgnoreCase(course.getQuizzes().getname())) {
-            course.getQuizzes().getname(newQuizName);
-            //do they waant to add a question, modify a question, change a answer?
-            course.getQuizzes().getquestion();
-        }
+    public void setCourses(ArrayList<Course> courses) {
+        this.courses = courses;
+    }
+    // will add all the courses they are in
+    public void addTeacherCourse(Course course) {
+        courses.add(course);
+    }
+    // adds questions to a quiz and then adds that quiz to a course
+    public String addQuiz(String course, String quizName, ArrayList<String> promt, ArrayList<String> answers, int answer) {
+        ArrayList<Question> questions = new ArrayList<>();
+        for (int k = 0; k < courses.size(); k++) {
+                for (int i = 0; i < promt.size(); i++) {
+                    questions.set(i, new Question(promt.get(i), answers.get(answer), answers));
+                }
+                if (course.equalsIgnoreCase(courses.get(k).getName())) {
+                    courses.get(k).addQuiz(new Quiz(quizName, questions));
+                }
+                return "it worked";
+            }
+        return "it did not work";
+    }
+    // modification 1 for quizzes
+    public String modifyQuizName(String course, int quizName, String newQuizName) {
+        for (int i = 0; i < courses.size(); i++)
+            if (course.equalsIgnoreCase(courses.get(i).getName())) {
+                    courses.get(i).getQuiz(quizName).setName(newQuizName);
+                
+                return "Quiz Name updated";
+            }
         return "Error. No quiz in this course by that name";
     }
-    public String removeQuiz() {
-        if (quizName.equalsIgnoreCase(course.getQuizzes().getname())) {
-            course.getQuizzes().getname("");
-            course.getQuizzes().setquestion(null);
-        }
+    // modification 2 for quizzes
+    public String modifyQuizPrompt(String course, int quizName, String prompt, String newprompt) {
+        for (int i = 0; i < courses.size(); i++)
+            if (course.equalsIgnoreCase(courses.get(i).getName())) {
+                courses.get(i).getQuiz(quizName).getQuestion(prompt).setPrompt(newprompt);
+                return "Question prompt updated";
+            }
+        return "Error. No quiz in this course by that name";
+    }
+    // modification 3 for quizzes
+    public String modifyQuizChoices(String course, int quizName,String prompt, ArrayList<String> choices) {
+        for (int i = 0; i < courses.size(); i++)
+            if (course.equalsIgnoreCase(courses.get(i).getName())) {
+                courses.get(i).getQuiz(quizName).getQuestion(prompt).setChoices(choices);
+                return "Question choices updated";
+            }
+        return "Error. No quiz in this course by that name";
+    }
+    // modification 4 for quizzes
+    public String modifyQuizAnswer(String course, int quizName,String prompt, String answer) {
+        for (int i = 0; i < courses.size(); i++)
+            if (course.equalsIgnoreCase(courses.get(i).getName())) {
+                courses.get(i).getQuiz(quizName).getQuestion(prompt).setAnswer(answer);
+                return "Question answer updated";
+            }
+        return "Error. No quiz in this course by that name";
+    }
+    // removes quiz from course
+    public String removeQuiz(String course) {
+        for (int i = 0; i < courses.size(); i++)
+            if (course.equalsIgnoreCase(courses.get(i).getName())) {
+                courses.get(i).removeQuiz(courses.get(i).getQuiz(i));
+                return "Removed Quiz";
+            }
         return "Error. No quiz in this course by that name";
     }
     // studentname = filename, need a String of what number quiz it is
-    public String checkStudentQuiz(String fileName, String quizName) {
-        String line = "";
-        try (BufferedReader br = new BufferedReader(new FileReader(fileName)) {
-            while ((line = br.readLine()) == null) {
-                if (line.equals(quizName)) {
-                    //print the amount of lines the quiz is
-                    for (int i = 0; i < 8; i++) {
-                        System.out.println(line = br.readLine());
-                    }
+    public ArrayList<String> checkStudentQuiz(String fileName, String quizName) {
+        ArrayList<String> studentQuiz = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            // will get the file and read it until the quiz is found.
+            while(true) {
+                if ((line = br.readLine()).equalsIgnoreCase(quizName)) {
                     break;
                 }
             }
-        } catch (FileNotFoundException e) {
-            throw e;
+            // finds when the quiz ends and adds each line of the quiz to a arraylist
+            while((line = br.readLine()).equalsIgnoreCase("end")) {
+                studentQuiz.add(line);
+            }
+            // returns a arraylist that can then be printed to show someones quiz
+            return studentQuiz;
+            
         } catch (Exception e) {
             e.printStackTrace();
+            studentQuiz.add("Did not work");
+            return studentQuiz;
         }
     }
+    // do quizzes get graded auto?
+    /** public String gradeQuiz() {
+     * we would need to have a list of completed quizzes or are we gradding each quiz per student(would rather do all)
+     * or each student that finished a quiz so i can look through their file and then check their answers with the answer
+     * i would then rewirte each file to get where they have the quiz and then under it put score, ones they got wrong,
+     * ones they got correct
+     * or add all the graded quizes to their own file for each quiz
+     */
 }
