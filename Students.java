aa87fa6f72d313cf881443e62bfc376fbf1816eb
aakarjain <75190201+aakarjain@users.noreply.github.com>
2022-04-07 17:41:55 -0400

    Update Students.java
    Tried to create vague framework for takeQuiz method

--- Students.java
+++ Students.java
@@ -1,7 +1,30 @@
-public class Students extends User {
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.sql.Timestamp;
 
+public class Student extends User {
 
     public Students(String name, String username, String password) {
         super(name, username, password, false);
     }
+
+    public ArrayList<String> takeQuiz(Quiz quiz, Scanner scan) {
+        ArrayList<String> responses;
+        for (int i = 0; i < quiz.getQuestions.size(); i++) {
+            Question currentQuestion = quiz.getQuestions.get(i);
+            System.out.println(currentQuestion.getPrompt());
+            for (int j = 0: j < currentQuestion.getChoices().size(); j++) {
+                System.out.println(currentQuestion.getChoice(j));
+            }
+            int responseOption = scan.nextInt();
+            responses.add(currentQuestion.getChoices().get(responseOption));
+        }
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println("Submitted!\n" + dateFormat.format(timestamp));
+        return responses;
+    }
+
+
 }
