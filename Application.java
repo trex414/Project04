be0ee6be60d14cabee67e577019214c3dc68e07c
trex414 <58962227+trex414@users.noreply.github.com>
2022-04-08 16:27:51 -0400

    Update Application.java
    Line 345 I removed the initialization of the String because I initialize it way before, but should not matter because it equals an empty string.

--- Application.java
+++ Application.java
@@ -342,7 +342,7 @@ public class Application {
                                         }
                                     }
 
-                                    String quizName = "";
+                                    quizName = "";
                                     boolean isQuiz = false;
                                     while (!isQuiz) {
 
