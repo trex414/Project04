e9cb7e9b7d17b40adc50eaaad020dfad1c157c10
Grayson Nocera <graysonnocera@pal-nat186-104-226.itap.purdue.edu>
2022-04-07 21:38:31 -0400

    new commit

--- Application.java
+++ Application.java
@@ -24,9 +24,10 @@ public class Application {
     public static String signUpSucc = "You are Signed-Up Successfully!";
     public static String existUser = "This is an existing Username!";
     public static String selOpt = "Select an option below:\n";
-    public static String chOpt = "1. Add Question\n2. Edit Quiz\n3. Delete Quiz\n4. View Student Submissions\n5. Exit";
+    public static String chOpt = "1. Add Quiz\n2. Edit Quiz\n3. Delete Quiz\n4. View Student Submissions\n5. Exit";
     public static String error = "Error! Please enter one of the options!\n";
-
+    public static String courseS = "1. Create course/n2. Join course";
+    public static String enterCourse = "Enter the name of the course: ";
 
     public static void main(String[] args) {
 
@@ -39,155 +40,180 @@ public class Application {
         // Hold users
         ArrayList<User> users = new ArrayList<User>();
 
-        // Welcome user, give user options to create a student account, 
-        // create a teacher account, log in, or exit
-        System.out.println(welcome);
-        System.out.println(loginP);
-        System.out.println(stuSignU);
-        System.out.println(teaSignU);
-        System.out.println(logIn);
-        System.out.println(exit);
+        while (true) {
 
-        // Store user's username and password
-        String userName;
-        String nPassword;
 
-        int optAns = sc.nextInt();
-        sc.nextLine();
+            // Welcome user, give user options to create a student account, 
+            // create a teacher account, log in, or exit
+            System.out.println(welcome);
+            System.out.println(loginP);
+            System.out.println(stuSignU);
+            System.out.println(teaSignU);
+            System.out.println(logIn);
+            System.out.println(exit);
 
-        // Verify user input
-        String prompt = String.format("%s\n%s\n%s\n%s\n%s\n", loginP, stuSignU, teaSignU, logIn, exit);
-        optAns = verifyInput(sc, 1, 4, prompt, optAns);
+            // Store user's username and password
+            String userName;
+            String nPassword;
 
-        if (optAns == 1) {
-            // Setting up students account
-            User user = createAccount(sc, usernames, false);
-            
-            // adding user
-            users.add(user);
-
-        } else if (optAns == 2) {
-
-            // setting up teachers account
-            User user = createAccount(sc, usernames, true);
-
-            // adding user
-            users.add(user);
-
-        } else if (optAns == 3) {
-
-            // Login
-            System.out.println(role);
-            System.out.println(roleSelection);
-
-            // Get selection: teacher, student
-            int rSelection = sc.nextInt();
+            int optAns = sc.nextInt();
             sc.nextLine();
 
-            // Verify input
-            String roleS = String.format("%s\n%s\n", role, roleSelection);
-            rSelection = verifyInput(sc, 1, 2, roleS, rSelection);
+            // Verify user input
+            String prompt = String.format("%s\n%s\n%s\n%s\n%s\n", loginP, stuSignU, teaSignU, logIn, exit);
+            optAns = verifyInput(sc, 1, 4, prompt, optAns);
 
-            if (rSelection == 1) {
-                //teacher account
-                do {
-                    System.out.println(username);
-                    userName = sc.nextLine();
-                    System.out.println(password);
-                    nPassword = sc.nextLine();
-                    if () {
-                        //verify username and password (doesn't match)
-                        System.out.println(verifyAcc);
-                        System.out.println(tryL);
+            if (optAns == 1) {
 
-                    } else if () {
-                        // if logged in as a different role
-                        System.out.println(verifUser);
-                        System.out.println(tryL);
+                // Setting up students account
+                User user = createAccount(sc, usernames, false);
+                
+                // adding user
+                users.add(user);
 
-                    } else {
-                        System.out.println(logSuccess);
-                        // continue for selection add quiz/ edit / delete/ view sub / Exit
-                        System.out.println(selOpt);
-                        System.out.println(chOpt);
-                        int optSel = sc.nextInt();
-                        sc.nextLine();
-                        if (optSel == 1) {
-                            //add quiz...
+            } else if (optAns == 2) {
 
-                        } else if (optSel == 2) {
-                            //edit...
+                // setting up teachers account
+                User user = createAccount(sc, usernames, true);
 
-                        } else if (optSel == 3) {
-                            //delete...
+                // adding user
+                users.add(user);
 
-                        } else if (optSel == 4) {
-                            //view submissions...
+            } else if (optAns == 3) {
 
-                        } else {
-                            //exit
-                            System.out.println(exitPrompt);
+                // Login
+                System.out.println(role);
+                System.out.println(roleSelection);
+
+                // Get selection: teacher, student
+                int rSelection = sc.nextInt();
+                sc.nextLine();
+
+                // Verify input
+                String roleS = String.format("%s\n%s\n", role, roleSelection);
+                rSelection = verifyInput(sc, 1, 2, roleS, rSelection);
+
+                boolean didLogin = false;
+
+                if (rSelection == 1) {
+
+                    //teacher account
+                    do {
+
+                        System.out.println(username);
+                        userName = sc.nextLine();
+                        System.out.println(password);
+                        nPassword = sc.nextLine();
+
+                        didLogin = loginUser(sc, userName, nPassword, users);
+
+                        if (!didLogin) {
+
+                            //verify username and password (doesn't match)
+                            System.out.println(verifyAcc);
+                            System.out.println(tryL);
                         }
-                    }
 
-                } while ()
-                //verify username and password
+                    } while (!didLogin);
 
-            } else if (rSelection == 2) {
-                //student account
-                do {
-                    System.out.println(username);
-                    user = sc.nextLine();
-                    System.out.println(password);
-                    pass = sc.nextLine();
-                    if () {
-                        //verify username and password (doesn't match)
-                        System.out.println(verifyAcc);
-                        System.out.println(tryL);
-                    } else if () {
-                        //verify whether its stu account or teacher's account (not sure)
-                        System.out.println(verifUser);
-                        System.out.println(tryL);
+                    // after they login
+                    System.out.println();
 
-                    } else {
-                        // if everything works matchs
-                        System.out.println(logSuccess);
-                        // continue for selection add quiz/ edit / delete/ view sub / Exit
-                        System.out.println(selOpt);
-                        System.out.println(chOpt);
-                        int optSel = sc.nextInt();
-                        sc.nextLine();
-                        if (optSel == 1) {
-                            //add quiz...
+                    System.out.println(selOpt);
+                    System.out.println(chOpt);
 
-                        } else if (optSel == 2) {
-                            //edit...
+                    // Options: 1 add quiz, 2 edit quiz, 3 delete quiz, 4 view submissions, 5 exit
 
-                        } else if (optSel == 3) {
-                            //delete...
+                } else if (rSelection == 2) {
 
-                        } else if (optSel == 4) {
-                            //view submissions...
+                    //teacher account
+                    do {
 
-                        } else {
-                            //exit
-                            System.out.println(exitPrompt);
+                        System.out.println(username);
+                        userName = sc.nextLine();
+                        System.out.println(password);
+                        nPassword = sc.nextLine();
+
+                        didLogin = loginUser(sc, userName, nPassword, users);
+
+                        if (!didLogin) {
+
+                            //verify username and password (doesn't match)
+                            System.out.println(verifyAcc);
+                            System.out.println(tryL);
                         }
-                    }
 
-                } while ()
-                //verify username and password
+                    } while (!didLogin);
 
+                    //student account
+                    // do {
+                    //     System.out.println(username);
+                    //     user = sc.nextLine();
+                    //     System.out.println(password);
+                    //     pass = sc.nextLine();
+                    //     if () {
+                    //         //verify username and password (doesn't match)
+                    //         System.out.println(verifyAcc);
+                    //         System.out.println(tryL);
+                    //     } else if () {
+                    //         //verify whether its stu account or teacher's account (not sure)
+                    //         System.out.println(verifUser);
+                    //         System.out.println(tryL);
+
+                    //     } else {
+                    //         // if everything works matchs
+                    //         System.out.println(logSuccess);
+                    //         // continue for selection add quiz/ edit / delete/ view sub / Exit
+                    //         System.out.println(selOpt);
+                    //         System.out.println(chOpt);
+                    //         int optSel = sc.nextInt();
+                    //         sc.nextLine();
+                    //         if (optSel == 1) {
+                    //             //add quiz...
+
+                    //         } else if (optSel == 2) {
+                    //             //edit...
+
+                        //     } else if (optSel == 3) {
+                        //         //delete...
+
+                        //     } else if (optSel == 4) {
+                        //         //view submissions...
+
+                        //     } else {
+                        //         //exit
+                        //         System.out.println(exitPrompt);
+                        //     }
+                        // }
+
+                }
+
+                // after they login
+
+
+            } else {
+
+                // Exit from the program
+                System.out.println(exitPrompt);
+                return;
             }
-
-        } else if (optAns == 4) {
-            // Exit from the program
-            System.out.println(exitPrompt);
         }
-
     }
 
-    public static boolean loginUser(Scanner sc, )
+    public static boolean loginUser(Scanner sc, String username, String password, ArrayList<User> users) {
+
+        boolean didLogin = false;
+
+        for (int i = 0; i < users.size(); i++) {
+
+            if (users.get(i).getUsername().equals(username) && users.get(i).getPassword().equals(password)) {
+                didLogin = true;
+                break;
+            }
+        }
+
+        return didLogin;
+    }
 
     public static User createAccount(Scanner sc, ArrayList<String> usernames, boolean isTeacher) {
 
@@ -221,7 +247,12 @@ public class Application {
         password = sc.nextLine();
         System.out.println(signUpSucc);
 
-        return new User(nFullName, userName, password, isTeacher);
+
+        if (isTeacher) {
+            return new Teacher(nFullName, userName, password, isTeacher);
+        }
+
+        return new Students(nFullName, userName, password, isTeacher);
     }
 
     public static int verifyInput(Scanner sc, int lowerB, int upperB, String prompt, int input) {
